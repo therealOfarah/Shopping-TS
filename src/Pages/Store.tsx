dc739a1393240ac94d32c31d39da85b540f53463
Omar Farah <omaarfaarah2019@gmail.com>
2022-10-26 15:11:53 -0400

    fixicon sizes

--- src/Pages/Store.tsx
+++ src/Pages/Store.tsx
@@ -3,10 +3,13 @@ import storeItems from "../data/items.json"
 import { StoreItem } from "../components/StoreItem"
 export function Store(){
   return(
+    <>
+    
     <Row xs={1} md={2} lg={3} className='g-3'>
       {storeItems.map(item=>(
       <Col key={item.id}><StoreItem {...item}/></Col>
       ))}
     </Row>
+    </>
   )
 }
--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -22,7 +22,7 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItem) {
       <Card.Img
         variant="top"
         src={imgUrl}
-        height="200px"
+        height="400px"
         style={{ objectFit: "cover" }}
       />
       <Card.Body className="d-flex flex-column">
